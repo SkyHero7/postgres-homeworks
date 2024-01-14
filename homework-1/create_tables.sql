2793eb9748051d4cfb19a51b4f85e5046c1211b0
Roman Makhnev <romanmahnef777@gmail.com>
2024-01-14 16:45:23 +0400

    Complete

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE IF NOT EXISTS employees (
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    title VARCHAR(100),
+    birth_date DATE,
+    notes TEXT
+);
+
+-- Создание таблицы "customers"
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id SERIAL PRIMARY KEY,
+    company_name VARCHAR(100),
+    contact_name VARCHAR(50)
+
+);
+
+-- Создание таблицы "orders"
+CREATE TABLE IF NOT EXISTS orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(customer_id),
+    employee_id INT REFERENCES employees(employee_id),
+    order_date DATE,
+    ship_city VARCHAR(50)
+
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,65 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+from psycopg2 import sql
+
+def execute_sql_query(query, values=None):
+    connection = psycopg2.connect(
+        user="postgres",
+        password="1234",
+        host="localhost",
+        port="5432",
+        database="north"
+    )
+
+    cursor = connection.cursor()
+    if values:
+        cursor.execute(query, values)
+    else:
+        cursor.execute(query)
+
+    connection.commit()
+    connection.close()
+
+
+# Чтение данных из файла и вставка в таблицу
+def create_tables():
+    with open("create_tables.sql", "r") as file:
+        create_tables_sql = file.read()
+        execute_sql_query(create_tables_sql)
+def record_exists(table_name, id_column_name, value):
+    connection = psycopg2.connect(
+        user="postgres",
+        password="1234",
+        host="localhost",
+        port="5432",
+        database="north"
+    )
+
+    cursor = connection.cursor()
+    query = f"SELECT COUNT(*) FROM {table_name} WHERE {id_column_name} = %s"
+
+    cursor.execute(query, (value,))
+    count = cursor.fetchone()[0]
+    connection.close()
+
+    return count > 0
+# Функция для вставки данных в таблицу
+def insert_data_into_table(table_name, csv_file_path):
+    with open(csv_file_path, "r", encoding="utf-8") as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            columns = ", ".join(row.keys())
+            values = ", ".join(["%s"] * len(row))
+
+            id_column_name = f"{table_name[:-1]}_id"  # Assuming that the table name ends with 's' (plural form)
+            id_value = row.get(id_column_name)
+
+            if not record_exists(table_name, id_column_name, id_value):
+                query = f"INSERT INTO {table_name} ({columns}) VALUES ({values})"
+                execute_sql_query(query, tuple(row.values()))
+
+create_tables()
+insert_data_into_table("employees", "north_data/employees_data.csv")
+# insert_data_into_table("customers", "north_data/customers_data.csv")
+# insert_data_into_table("orders", "north_data/orders_data.csv")
+# #
