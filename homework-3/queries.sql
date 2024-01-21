097be27d05135ebca8313a1fc9bd16d3a64776f0
Roman Makhnev <romanmahnef777@gmail.com>
2024-01-21 13:15:18 +0400

    Complete homework 3-4

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,32 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
+SELECT customers.company_name, CONCAT(employees.first_name, ' ', employees.last_name) AS employee_name
+FROM customers
+JOIN employees ON customers.city = 'London' AND employees.city = 'London'
+JOIN shippers ON customers.shipper_id = shippers.shipper_id AND shippers.company_name = 'United Package';
 
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT products.product_name, products.units_in_stock, suppliers.contact_name, suppliers.phone
+FROM products
+JOIN suppliers ON products.supplier_id = suppliers.supplier_id
+WHERE products.discontinued = false AND products.units_in_stock < 25
+    AND products.category IN ('Dairy Products', 'Condiments')
+ORDER BY products.units_in_stock;
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT customers.company_name
+FROM customers
+LEFT JOIN orders ON customers.customer_id = orders.customer_id
+WHERE orders.order_id IS NULL;
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT DISTINCT products.product_name
+FROM products
+JOIN order_details ON products.product_id = order_details.product_id
+WHERE order_details.quantity = 10;
